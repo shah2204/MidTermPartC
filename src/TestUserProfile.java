c8a6c89e6d9203dd7a00d6dfdccf437f4cbb251b
Dell <Dell@aDmin>
2022-02-23 11:05:22 -0500

    Objects are added.

---
+++ src/TestUserProfile.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+
+/**
+ *
+ * @author Dell
+ */
+public class TestUserProfile {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name=sc.nextLine();
+        System.out.println("Choose one Genre number");
+        int gen=sc.nextInt();
+    }
+    
+}
